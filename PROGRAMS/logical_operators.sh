ec34396a974f413984d06b8bff0c820232d80d18
Micah Joseph <72503783+micahjoseph1234@users.noreply.github.com>
2026-06-12 17:53:35 +0530

    Add logical_operators.sh for number range checks

---
+++ PROGRAMS/logical_operators.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo -n "Enter a NUM: "
+read NUM
+
+# Check whether a number is between 10 and 20 (Using AND -a operator)
+if [ $NUM -ge 10 -a $NUM -le 20 ]
+then
+    echo "$NUM is between 10 and 20"
+else
+    echo "$NUM is NOT between 10 and 20"
+fi
+
+echo -n "Enter another NUM: "
+read NUM
+
+# Check whether a number is between 10 and 20 (Using OR -o operator)
+if [ $NUM -lt 10 -o $NUM -gt 20 ]
+then
+    echo "$NUM is NOT between 10 and 20"
+else
+    echo "$NUM is between 10 and 20"
+fi
