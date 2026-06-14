5078fecc27e5e761ef8a512a61afd89d9365c4ad
Micah Joseph <72503783+micahjoseph1234@users.noreply.github.com>
2026-06-15 02:16:22 +0530

    Create generic_package_installer.sh

---
+++ DAY 3/PROGRAMS /generic_package_installer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+check_and_install()
+{
+    if command -v $1 >/dev/null 2>&1
+    then
+        echo "$1 already installed"
+    else
+        echo "Installing $2..."
+        sudo apt install $2 -y
+    fi
+}
+
+check_and_install git git
+check_and_install java default-jdk
+check_and_install mvn maven
